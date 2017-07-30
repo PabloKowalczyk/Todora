c07047d02054563027b27a7cf3a6b6fcfa76433a
pkowalczyk <pkowalczyk90@gmail.com>
2017-07-30 14:41:32 +0200

    Require ext-apcu.

--- vagrant/apt.sh
+++ vagrant/apt.sh
@@ -4,10 +4,21 @@ export DEBIAN_FRONTEND=noninteractive;
 
 PHP_VERSION="7.1";
 
-## Add Onrej PPA for php 5.6 - 7.1
+## Add Onrej PPA for php 5.6 - 7.2
 add-apt-repository -y -u ppa:ondrej/php > /dev/null 2>&1;
 
-apt-get install git "php$PHP_VERSION" "php$PHP_VERSION-zip" "php$PHP_VERSION-mbstring" \
-    "php$PHP_VERSION-opcache" "php$PHP_VERSION-xml" "php$PHP_VERSION-pgsql" "php$PHP_VERSION-gd" \
-    "php$PHP_VERSION-intl" "php$PHP_VERSION-fpm" htop curl build-essential \
+apt-get install git \
+    "php$PHP_VERSION" \
+    "php$PHP_VERSION-zip" \
+    "php$PHP_VERSION-mbstring" \
+    "php$PHP_VERSION-opcache" \
+    "php$PHP_VERSION-xml" \
+    "php$PHP_VERSION-pgsql" \
+    "php$PHP_VERSION-gd" \
+    "php$PHP_VERSION-intl" \
+    "php$PHP_VERSION-fpm" \
+    php-apcu \
+    htop \
+    curl \
+    build-essential \
     redis-server -y > /dev/null;
