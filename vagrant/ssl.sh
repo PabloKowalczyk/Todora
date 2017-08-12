5b08ac81d4f399a3a73b0804e608e940250e9089
pkowalczyk <pkowalczyk90@gmail.com>
2017-08-12 12:14:01 +0200

    Fixed typo.

--- vagrant/ssl.sh
+++ vagrant/ssl.sh
@@ -14,4 +14,4 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/stage
     -out /etc/nginx/ssl/stage.todora.dev.crt \
     -subj "/C=PL/ST=Masovian/L=Warsaw/O=PK Ltd/OU=IT Department/CN=stage.todora.dev" > /dev/null 2>&1 &&
 
-echo "- SSL cert generated for prod env.";
+echo "- SSL cert generated for stage env.";
