636955059bce111a894c6dbbdcc72c9be564c3b0
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-01-12 22:27:34 +0100

    Updated node to v9 and added global yarn

--- vagrant/nodejs.sh
+++ vagrant/nodejs.sh
@@ -2,8 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive;
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - > /dev/null
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash - > /dev/null
 apt-get -qq install -y nodejs > /dev/null
 
-npm install -sg npm@latest > /dev/null
-npm install -sg gulp > /dev/null
+npm install -sg yarn > /dev/null
