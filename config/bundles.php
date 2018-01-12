51747ab635249d1385e0dcc41c9fa522de18fc3c
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-01-12 22:26:42 +0100

    Removed DoctrineMigrations

--- config/bundles.php
+++ config/bundles.php
@@ -7,7 +7,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Matthimatiker\OpcacheBundle\MatthimatikerOpcacheBundle::class => ['dev' => true, 'test' => true],
