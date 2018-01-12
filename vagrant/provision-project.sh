15eed1855a9692dc80e866f4b1106813401e60c4
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-01-12 23:11:28 +0100

    Removed fixtures

--- vagrant/provision-project.sh
+++ vagrant/provision-project.sh
@@ -3,4 +3,3 @@
 (cd /vagrant && composer install -o -q -n --prefer-dist);
 (cd /vagrant && bin/console doctrine:database:create -e dev --if-not-exists -q -n);
 (cd /vagrant && bin/console doctrine:schema:create -e dev -n -q);
-(cd /vagrant && bin/console doctrine:fixtures:load --fixtures=fixtures -n -q);
