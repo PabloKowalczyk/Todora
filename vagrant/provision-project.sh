fb513d0e47e7740e9a0aed1761586f0f272692a3
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-01-13 11:20:26 +0100

    Install bin dependencies.

--- vagrant/provision-project.sh
+++ vagrant/provision-project.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 (cd /vagrant && composer install -o -q -n --prefer-dist);
+(cd /vagrant && composer bin all install -a -q -n --prefer-dist);
 (cd /vagrant && bin/console doctrine:database:create -e dev --if-not-exists -q -n);
 (cd /vagrant && bin/console doctrine:schema:create -e dev -n -q);
