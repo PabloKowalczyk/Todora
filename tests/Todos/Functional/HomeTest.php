04831bc9fbd2374963529a6459a3b34500e1950e
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-01-28 21:48:27 +0100

    Changed test file name.

--- tests/Todos/Functional/HomeTest.php
+++ tests/Todos/Functional/HomeTest.php
@@ -7,23 +7,14 @@ namespace Todora\Tests\Todos\Functional;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
-class BoardTest extends WebTestCase
+class HomeTest extends WebTestCase
 {
     /** @test */
-    public function boardIsAccessible(): void
+    public function pageIsAccessible(): void
     {
         $client = self::createClient();
 
-        $client->request(
-            'get',
-            '/',
-            [],
-            [],
-            [
-                'PHP_AUTH_USER' => 'admin',
-                'PHP_AUTH_PW' => $_ENV['ADMIN_PASSWORD'],
-            ]
-        );
+        $client->request('get', '/');
 
         /** @var Response $response */
         $response = $client->getResponse();
