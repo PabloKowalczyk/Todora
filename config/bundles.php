b4428abb63d1c571024618cec1e19f178198d4b3
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-01-28 16:22:38 +0100

    Enable MonologBundle.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
