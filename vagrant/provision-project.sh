9371cf0053a1a118890e4b8e190e05c25b18c3aa
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-02-03 23:06:09 +0100

    Added fixture packages and migrate dev/test dbs.

--- vagrant/provision-project.sh
+++ vagrant/provision-project.sh
@@ -2,5 +2,5 @@
 
 (cd /vagrant && composer install -o -q -n --prefer-dist);
 (cd /vagrant && composer bin all install -a -q -n --prefer-dist);
-(cd /vagrant && bin/console doctrine:database:create -e dev --if-not-exists -q -n);
-(cd /vagrant && bin/console doctrine:schema:create -e dev -n -q);
+(cd /vagrant && composer todora:migrate:dev -q -n);
+
