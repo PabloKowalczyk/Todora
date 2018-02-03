396c6eb4973fb4b077ef9f86bffdf21ed04a8a3a
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-02-03 23:55:08 +0100

    Added missing HomeAction.

---
+++ src/Todos/UserInterface/HomeAction.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Todora\Todos\UserInterface;
+
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeAction
+{
+    /**
+     * @var EngineInterface
+     */
+    private $engine;
+
+    public function __construct(EngineInterface $engine)
+    {
+        $this->engine = $engine;
+    }
+
+    public function __invoke(): Response
+    {
+        return $this->engine
+            ->renderResponse('pages/home.html.twig');
+    }
+}
