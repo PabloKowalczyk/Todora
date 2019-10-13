08d522ef891f8569d99a46881c05621f8bc2a53c
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2019-10-13 09:28:36 +0200

    Disable "PixersDoctrineProfilerBundle" and "PabloKCacheStatsBundle" in "test" environment

--- config/bundles.php
+++ config/bundles.php
@@ -9,7 +9,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Pixers\DoctrineProfilerBundle\PixersDoctrineProfilerBundle::class => ['dev' => true, 'test' => true],
+    Pixers\DoctrineProfilerBundle\PixersDoctrineProfilerBundle::class => ['dev' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
@@ -17,5 +17,5 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
-    PabloK\CacheStatsBundle\PabloKCacheStatsBundle::class => ['dev' => true, 'test' => true],
+    PabloK\CacheStatsBundle\PabloKCacheStatsBundle::class => ['dev' => true],
 ];
